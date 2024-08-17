7e87225fd2e851fe3a0775495d097fffecdbf5ce
ajay2603 <j.a.r.raju2003@gmail.com>
2024-08-17 22:10:04 +0530

    keywords edited.

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,7 @@ export const metadata = {
   description:
     "Hi there! I'm Atchuta Rama Raju Jampana, a passionate Full Stack Developer specializing in the MERN stack, Next.js, and cross-platform mobile app development with React Native and Flutter. I create dynamic, responsive websites and mobile apps with clean code and optimized performance.",
   keywords:
-    "Full Stack Developer, MERN stack, Next.js, React Native, Flutter, Cross-Platform Development, Portfolio, Atchuta Rama Raju Jampana, Web Development, JavaScript, TypeScript, My Quiz, Pigeon, Safe Sprout, Child Tracking, Mobile App Development",
+    "Full Stack Developer, MERN stack, Next.js, React Native, Flutter, Cross-Platform Development, Portfolio, Atchuta Rama Raju Jampana, jarraju, ajay, ajay2603 Web Development, JavaScript, TypeScript, My Quiz, Pigeon, Safe Sprout, Child Tracking, Mobile App Development",
 };
 
 export default function RootLayout({ children }) {
